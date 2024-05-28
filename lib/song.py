de3ad45d209f3e31328adc84c7e6e9def8381069
hildamu <mukamihilda437@gmail.com>
2024-05-28 11:00:42 +0300

    submit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,18 @@
 class Song:
-    pass
+    count = 0
+    genre_count = {}
+    artist_count = {}
+    genres = set()
+    artists = set()
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        Song.genre_count.setdefault(self.genre, 0)
+        Song.genre_count[self.genre] += 1
+        Song.artist_count.setdefault(self.artist, 0)
+        Song.artist_count[self.artist] += 1
+        Song.genres.add(self.genre)
+        Song.artists.add(self.artist)
